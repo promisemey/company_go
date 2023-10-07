e1849d48cc6e04903715072881d74b3cc9bb6e8c
CouMe <2636699780@qq.com>
2023-10-07 19:44:20 +0800

    首页布局完善

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -35,17 +35,80 @@
 		<!-- 宫格 -->
 
 		<!-- 为你推荐 -->
-		<view class="padding-left-lg margin-left-xs ">
-			<view class="flex align-center justify-between padding-right-lg margin-right-xs margin-bottom-sm">
+		<view class=" ">
+			<view class="flex align-center justify-between padding-left-xl margin-right-sm  margin-bottom-sm">
 				<text class="text-lg text-black">为你推荐</text>
 				<text class="text-orange">查看全部</text>
 			</view>
-			<view class="flex">
-				<job-item />
-				<job-item />
-			</view>
+			<scroll-view scroll-x class="margin-right-xl">
+				<view class="flex justify-between margin-left-xl">
+					<job-item v-for="item in 4" class="margin-right-lg shrink" />
+				</view>
+			</scroll-view>
 		</view>
 		<!-- 为你推荐 -->
+
+		<!-- 热门职位 -->
+		<view class="hot margin-top-sm">
+			<view class="flex align-center justify-between padding-left-xl margin-right-sm  margin-bottom-sm">
+				<text class="text-lg text-black">热门职位</text>
+				<text class="text-orange">查看全部</text>
+			</view>
+			<scroll-view scroll-x class="margin-right-xl">
+				<view class="flex justify-between margin-left-xl">
+					<job-item v-for="item in 4" class="margin-right-lg shrink" />
+				</view>
+			</scroll-view>
+		</view>
+		<!-- 热门职位 -->
+
+		<!-- 最新职位 -->
+		<view class="hot margin-top-sm">
+			<view class="flex align-center justify-between padding-left-xl margin-right-sm  margin-bottom-sm">
+				<text class="text-lg text-black">最新职位</text>
+				<text class="text-orange">查看全部</text>
+			</view>
+
+			<view class="flex flex-wrap padding-left-xl padding-right-sm ">
+				<job-item v-for="item in 3" state="new" class="margin-bottom-lg" />
+			</view>
+
+		</view>
+		<!-- 最新职位 -->
+
+		<!-- 最受关注 -->
+		<view class="hot margin-top-sm">
+			<view class="flex align-center justify-between padding-left-xl margin-right-sm  margin-bottom-sm">
+				<text class="text-lg text-black">最受关注</text>
+				<text class="text-orange">查看全部</text>
+			</view>
+
+			<view class="flex flex-wrap padding-left-xl padding-right-sm ">
+				<view v-for="item in 5"
+					:class="['flex align-center padding-tb-sm justify-between hot-list margin-top-xs',,item==5?'':'solid-bottom']">
+					<!-- 公司头像 -->
+					<image class="cu-avatar round sm " src="../../static/logo.png" mode="aspectFill"></image>
+					<!-- 公司头像 -->
+
+					<view class="company-desc basis-lg">
+						<view class="text-lg text-black ">
+							公司名称示例文字
+						</view>
+						<view class="text-sm text-grey ">
+							北京,海淀
+						</view>
+					</view>
+					<!-- 浏览量 -->
+					<view class="flex align-center">
+						<text class="margin-right-xs iconfont icon-yanjing1 text-grey"></text>
+						40k
+					</view>
+					<!-- 浏览量 -->
+				</view>
+			</view>
+
+		</view>
+		<!-- 最受关注 -->
 	</view>
 </template>
 
@@ -83,5 +146,31 @@
 	.grid-icon {
 		width: 64upx;
 		height: 64upx;
+	}
+
+	.text-black {
+		font-weight: 700;
+	}
+
+	.shrink {
+		flex-shrink: 0 !important;
+	}
+
+	.hot-list {
+		// width: 327px;
+		width: 100%;
+		height: 128upx;
+	}
+
+	.cu-avatar {
+		align-self: flex-start;
+		margin-top: 14upx;
+	}
+
+	.company-desc {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
 	}
 </style>
